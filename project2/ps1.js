11b4d010aa14cc5652df43120a34b8aefb9338d0
Mujti-Alsaihati <76984029+Mujti-Alsaihati@users.noreply.github.com>
2021-03-18 04:52:39 -0700

    updated

---
+++ project2/ps1.js
@@ -0,0 +1,39 @@
+$('#ps1').click(function(){
+    $('.ps1-content').css('display', 'block')
+    $('.ps2-content').css('display', 'none')
+    $('.ps3-content').css('display', 'none')
+    $('.ps4-content').css('display', 'none')
+    $('.ps5-content').css('display', 'none')
+  })
+  
+  $('#ps2').click(function(){
+    $('.ps1-content').css('display', 'none')
+    $('.ps2-content').css('display', 'block')
+    $('.ps3-content').css('display', 'none')
+    $('.ps4-content').css('display', 'none')
+    $('.ps5-content').css('display', 'none')
+  })
+  
+  $('#ps3').click(function(){
+    $('.ps1-content').css('display', 'none')
+    $('.ps2-content').css('display', 'none')
+    $('.ps3-content').css('display', 'block')
+    $('.ps4-content').css('display', 'none')
+    $('.ps5-content').css('display', 'none')
+  })
+  
+  $('#ps4').click(function(){
+    $('.ps1-content').css('display', 'none')
+    $('.ps2-content').css('display', 'none')
+    $('.ps3-content').css('display', 'none')
+    $('.ps4-content').css('display', 'block')
+    $('.ps5-content').css('display', 'none')
+  })
+  
+  $('#ps5').click(function(){
+    $('.ps1-content').css('display', 'none')
+    $('.ps2-content').css('display', 'none')
+    $('.ps3-content').css('display', 'none')
+    $('.ps4-content').css('display', 'none')
+    $('.ps5-content').css('display', 'block')
+  })
